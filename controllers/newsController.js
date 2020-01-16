a6d3f8618b5a432da2f8a65a89ef0420031808b7
JC Lenz <jc@miserablepop.com>
2020-01-15 23:14:07 -0800

    Added routes for getting and removing the articles from the db

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -2,8 +2,10 @@ var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Setting the middleware
 var router = express.Router();
 
+// Require all models
 var db = require("../models");
 
 
@@ -15,6 +17,7 @@ router.get("/saved", function(req, res){
     res.render("saved");
 });
 
+ // A GET route for scraping the NPR site
 router.get("/scrape", function(req, res){
 
     axios.get("https://www.npr.org/").then(function(response){
@@ -55,6 +58,34 @@ router.get("/scrape", function(req, res){
 
         // res.send("Scrape Complete");
     });
+});
+
+// Route for getting all Articles from the db
+app.get("/api/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for removing all Articles from the db
+app.delete("/api/articles", function (req, res) {
+    // Remove every document in the Articles collection
+    db.Article.remove({})
+        .then(function (dbArticle) {
+            // If we were able to successfully remove Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
 // Export routes for server.js to use.
