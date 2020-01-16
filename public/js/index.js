8e0c3795006d0f52780165734ad11d932ae3c38c
JC Lenz <jc@miserablepop.com>
2020-01-15 23:32:33 -0800

    Adding JS directory with files to handle jquery and other functions

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.fixed-action-btn').floatingActionButton();
+    $("#scrapeResults").empty();
+    initArticles();
+});
