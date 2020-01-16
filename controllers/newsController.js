e7c1cbd041d4e6fd7b976b2875a397047daaec0b
JC Lenz <jc@miserablepop.com>
2020-01-16 00:13:43 -0800

    Added routes for posting a new saved article, retrieving an article and populating with its note, and for saving/updating artcile with it's note

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -9,6 +9,9 @@ var router = express.Router();
 var db = require("../models");
 
 
+////////////////////////////////////////////////////////////////////////////////
+// HTML Routes
+////////////////////////////////////////////////////////////////////////////////
 router.get("/", function(req, res){
     res.render("index");
 });
@@ -17,7 +20,12 @@ router.get("/saved", function(req, res){
     res.render("saved");
 });
 
- // A GET route for scraping the NPR site
+
+////////////////////////////////////////////////////////////////////////////////
+// API Routes
+////////////////////////////////////////////////////////////////////////////////
+
+// A GET route for scraping the NPR site
 router.get("/api/scrape", function(req, res){
 
     axios.get("https://www.npr.org/").then(function(response){
@@ -130,7 +138,7 @@ router.get("/api/saved", function (req, res) {
         });
 });
 
-//Route to find specific article for saving
+// Route to find specific article for saving
 router.get("/api/articles/:id", function (req, res) {
     db.Article.findOne({ _id: req.params.id })
         .then(function (dbArticle) {
@@ -144,7 +152,7 @@ router.get("/api/articles/:id", function (req, res) {
         });
 });
 
-//Route to find specific article for saving
+// Route to find specific article for saving
 router.get("/api/saved/:id", function (req, res) {
     db.Save.findOne({ _id: req.params.id })
     .populate("note")
@@ -159,7 +167,7 @@ router.get("/api/saved/:id", function (req, res) {
         });
 });
 
-//Route to find specific article for saving
+// Route to find specific article for saving
 router.delete("/api/articles/:id", function (req, res) {
     db.Article.remove({ _id: req.params.id })
         .then(function (dbArticle) {
@@ -172,7 +180,7 @@ router.delete("/api/articles/:id", function (req, res) {
         });
 });
 
-//Route to find specific article for saving
+// Route to find specific article for saving
 router.delete("/api/notes/:id", function (req, res) {
     db.Note.remove({ _id: req.params.id })
         .then(function (dbNote) {
@@ -185,11 +193,66 @@ router.delete("/api/notes/:id", function (req, res) {
         });
 });
 
-//Route to find specific article for saving
+// Route to find specific article for saving
 router.delete("/api/saved/:id", function (req, res) {
     db.Save.remove({ _id: req.params.id })
         .then(function (dbArticle) {
             // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for posting a new saved article
+router.post("/api/saved", function (req, res) {
+    console.log(req.body);
+
+    var result = {};
+
+    result.title = req.body.title;
+    result.link = req.body.link;
+
+    db.Save.create(result)
+    .then(function (dbSaved) {
+        // View the added result in the console
+        console.log(dbSaved);
+    })
+    .catch(function (err) {
+        // If an error occurred, log it
+        console.log(err);
+    });
+});
+
+
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for saving/updating an Article's associated Note
+router.post("/api/notes/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            
+            return db.Save.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
             res.json(dbArticle);
         })
         .catch(function (err) {
