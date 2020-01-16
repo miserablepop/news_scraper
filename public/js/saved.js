0df52a9d2b194fc47eac4583d6c6dcef545ced2c
JC Lenz <jc@miserablepop.com>
2020-01-16 01:03:14 -0800

    Added variable API for AJAX calls in saved.js

--- public/js/saved.js
+++ public/js/saved.js
@@ -7,3 +7,91 @@ $(document).ready(function () {
 
     initSaved();
 });
+
+var API = {
+    getScrape: function () {
+        return $.ajax({
+            url: "api/scrape",
+            type: "GET"
+        });
+    },
+    init: function () {
+        return $.ajax({
+            url: "api/articles",
+            type: "GET"
+        });
+    },
+    getSaves: function () {
+        return $.ajax({
+            url: "api/saved",
+            type: "GET"
+        });
+    },
+    getArticle: function (id) {
+        return $.ajax({
+            url: "api/articles/" + id,
+            type: "GET"
+        });
+    },
+    getSavedArticle: function (id) {
+        return $.ajax({
+            url: "api/saved/" + id,
+            type: "GET",
+        });
+    },
+    getNote: function (id) {
+        return $.ajax({
+            url: "api/notes/" + id,
+            type: "GET"
+        });
+    },
+    save: function (article) {
+        return $.ajax({
+            headers: {
+                "Content-Type": "application/json"
+            },
+            type: "POST",
+            url: "api/saved",
+            data: JSON.stringify(article)
+        });
+    },
+    deleteArticle: function (id) {
+        return $.ajax({
+            url: "api/articles/" + id,
+            type: "DELETE"
+        });
+    },
+    deleteSaved: function (id) {
+        return $.ajax({
+            url: "api/saved/" + id,
+            type: "DELETE"
+        });
+    },
+    deleteNoteYeah: function (id) {
+        return $.ajax({
+            url: "api/notes/" + id,
+            type: "DELETE"
+        });
+    },
+    deleteAllSavedArt: function () {
+        return $.ajax({
+            url: "api/saved/",
+            type: "DELETE"
+        });
+    },
+    deleteAllNotes: function () {
+        return $.ajax({
+            url: "api/notes/",
+            type: "DELETE"
+        });
+    },
+    saveNote: function (id, note) {
+        return $.ajax({
+            url: "api/notes/" + id,
+            type: "POST",
+            data: {
+                body: note
+            }
+        });
+    }
+};
