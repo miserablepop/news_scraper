bbc7e14bbbd385b6e5a957f995e1a28b95e140c5
JC Lenz <jc@miserablepop.com>
2020-01-16 00:59:19 -0800

    Adding jQuery functionality to saved.js

---
+++ public/js/saved.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.fixed-action-btn').floatingActionButton();
+    // $("#savedResults").empty();
+    $('.modal').modal();
+
+    initSaved();
+});
